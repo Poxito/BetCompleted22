005ee0eed98e07178705ea4c904e255d464c6d17
aiveni <avelaz003@ikasle.ehu.eus>
2022-10-31 12:16:16 +0100

    gertaeraksortuINT azken aldaketa txika

--- src/test/java/test/gertaerakSortu/GertaerakSortuINTTest.java
+++ src/test/java/test/gertaerakSortu/GertaerakSortuINTTest.java
@@ -36,13 +36,9 @@ public class GertaerakSortuINTTest {
 	BLFacade sut = new BLFacadeImplementation(da);
 	
 	@Test
-	// sut.gertaerakSortu: The event does not exist and it is introduced.
+	// Eventua sortzen da.
 	public void test1() {
 		try {
-
-			String kirola = "Tennis";
-			String deskr = "Nadal-Federer";
-
 			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 			Date data = null;
 			try {
@@ -51,26 +47,19 @@ public class GertaerakSortuINTTest {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
 			Mockito.when(da.gertaerakSortu(Mockito.anyString(), Mockito.any(), Mockito.any())).thenReturn(true);
-			
 			boolean esperotakoa = true;
-			boolean emaitza = sut.gertaerakSortu(deskr, data, kirola);
-
+			boolean emaitza = sut.gertaerakSortu("Nadal-Federer", data, "Tennis");
 			assertEquals(esperotakoa, emaitza);
 		} catch (Exception e) {
-			// if the program goes to this point fail
 			fail();
 		}
 	}
 	
 	@Test
-	// sut.gertaerakSortu: The description is null.
+	// Ez dauka deskripziorik.
 	public void test2() {
 		try {
-
-			String kirola = "Tennis";
-
 			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 			Date data = null;
 
@@ -95,12 +84,9 @@ public class GertaerakSortuINTTest {
 	}
 	
 	@Test
-	// sut.gertaerakSortu: The date is null.
+	// data null danean.
 	public void test3() {
 		try {
-
-			String kirola = "Tennis";
-			String deskr = "Nadal-Federer";
 			
 			Mockito.when(da.gertaerakSortu(Mockito.anyString(), null, Mockito.any())).thenThrow(new RuntimeException());
 
@@ -118,17 +104,14 @@ public class GertaerakSortuINTTest {
 	}
 	
 	@Test
-	// sut.gertaerakSortu: The sport is null.
+	// sport null.
 	public void test4() {
 		try {
-
-			String deskr = "Nadal-Federer";
-
-			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 			Date data = null;
 
 			try {
-				data = sdf.parse("17/11/2022");
+				data = format.parse("17/11/2022");
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -150,13 +133,9 @@ public class GertaerakSortuINTTest {
 	}
 	
 	@Test
-	// sut.gertaerakSortu: The sport is not in the BD.
+	// sport ez dago DBan.
 	public void test5() {
 		try {
-
-			String kirola = "Xakea";
-			String deskr = "Donostia-Eibar";
-
 			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 			Date data = null;
 
@@ -170,7 +149,7 @@ public class GertaerakSortuINTTest {
 			Mockito.when(da.gertaerakSortu(Mockito.anyString(), Mockito.any(), Mockito.any())).thenReturn(false);
 
 			boolean esperotakoa = false;
-			boolean emaitza = sut.gertaerakSortu(deskr, data, kirola);
+			boolean emaitza = sut.gertaerakSortu("proba-probatzen", data, "pianoa");
 
 			assertEquals(esperotakoa, emaitza);
 		} catch (Exception e) {
@@ -180,13 +159,9 @@ public class GertaerakSortuINTTest {
 	}
 	
 	@Test
-	// sut.gertaerakSortu: Already exists
+	// Jadanik existitzen da.
 	public void test6() {
 		try {
-
-			String kirola = "Baloncesto";
-			String deskr = "LA Lakers-Phoenix Suns";
-
 			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 			Date data = null;
 
@@ -200,7 +175,7 @@ public class GertaerakSortuINTTest {
 			Mockito.when(da.gertaerakSortu(Mockito.anyString(), Mockito.any(), Mockito.any())).thenReturn(false);
 
 			boolean esperotakoa = false;
-			boolean emaitza = sut.gertaerakSortu(deskr, data, kirola);
+			boolean emaitza = sut.gertaerakSortu("LA Lakers-Phoenix Suns", data, "Baloncesto");
 
 			assertEquals(esperotakoa, emaitza);
 		} catch (Exception e) {
@@ -209,90 +184,5 @@ public class GertaerakSortuINTTest {
 		}
 	}
 	
-	@Test
-	// sut.gertaerakSortu: The date is yesterday
-	public void test7_1() {
-		try {
-
-			String kirola = "Tennis";
-			String deskr = "Nadal-Federer";
-
-			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-			Date data = null;
-			
-			try {
-				data = sdf.parse("25/10/2022");
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			boolean esperotakoa = false;
-			boolean emaitza = sut.gertaerakSortu(deskr, data, kirola);
-			fail();
-
-			//assertEquals(esperotakoa, emaitza);	
-		} catch (EventFinished e) {
-			assertTrue(true);
-		} 
-	}
-	
-	@Test
-	// sut.gertaerakSortu: The date is today
-	public void test7_2() {
-		try {
-
-			String kirola = "Tennis";
-			String deskr = "Nadal-Federer";
-
-			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-			Date data = null;
-			
-			try {
-				data = sdf.parse("26/10/2022");
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-						
-			boolean esperotakoa = false;
-			boolean emaitza = sut.gertaerakSortu(deskr, data, kirola);
-			fail();
-			
-			//assertEquals(esperotakoa, emaitza);
-		} catch (EventFinished e) {
-			assertTrue(true);
-		}
-	}
-	
-	@Test
-	// sut.gertaerakSortu: The date is tomorrow
-	public void test7_3() {
-		try {
-
-			String kirola = "Tennis";
-			String deskr = "Nadal-Federer";
-
-			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-			Date data = null;
-			
-			try {
-				data = sdf.parse("27/10/2022");
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			Mockito.when(da.gertaerakSortu(Mockito.anyString(), Mockito.any(), Mockito.any())).thenReturn(true);
-
-			boolean esperotakoa = true;
-			boolean emaitza = sut.gertaerakSortu(deskr, data, kirola);
-
-			//assertEquals(esperotakoa, emaitza);
-		} catch (Exception e) {
-			// if the program goes to this point fail
-			fail();
-		}
-	}
 
 }
